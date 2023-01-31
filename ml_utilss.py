1c8fd086899eb7f541594e12b78ecdc0a9a2d533
zareeian <zareeian@ualberta.ca>
2023-01-31 16:15:51 -0700

    1

---
+++ ml_utilss.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import math
+import sklearn.datasets
+import ipywidgets as widgets
+
+##Seaborn for fancy plots. 
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.rcParams["figure.figsize"] = (10,10)
+
+class edaDF:
+    """
+    A class used to perform common EDA tasks
+
+    ...
+
+    Attributes
+    ----------
+    data : dataframe
+        a dataframe on which the EDA will be performed
+    target : str
+        the name of the target column
+    cat : list
+        a list of the names of the categorical columns
+    num : list
+        a list of the names of the numerical columns
+
+    Methods
+    -------
+    setCat(catList)
+        sets the cat variable listing the categorical column names to the list provided in the argument catList
+        
+        Parameters
+        ----------
+        catlist : list
+            The list of column names that are categorical
+
+    setNum(numList)
+        sets the cat variable listing the categorical column names to the list provided in the argument catList
+        
+        Parameters
+        ----------
+        numlist : list
+            The list of column names that are numerical
+
+    countPlots(self, splitTarg=False, show=True)
+        generates countplots for the categorical variables in the dataset 
+
+        Parameters
+        ----------
+        splitTarg : bool
+            If true, use the hue function in the countplot to split the data by the target value
+        show : bool
+            If true, display the graphs when the function is called. Otherwise the figure is returned.
+    
+    histPlots(self, splitTarg=False, show=True)
+        generates countplots for the categorical variables in the dataset 
+
+        Parameters
+        ----------
+        splitTarg : bool
+            If true, use the hue function in the countplot to split the data by the target value
+        show : bool
+            If true, display the graphs when the function is called. Otherwise the figure is returned. 
+
+    fullEDA()
+        Displays the full EDA process. 
+    """
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+        self.cat = []
+        self.num = []
+
+    def info(self):
+        return self.data.info()
+
+    def describe(self):
+        print(self.data.describe())
+    
+    def isna(self):
+        print(self.data.isna().sum())
+
+    def giveTarget(self):
+        return self.target
+        
+    def setCat(self, catList):
+        self.cat = catList
+    
+    def setNum(self, numList):
+        self.num = numList
+
+    def countPlots(self, splitTarg=False, show=True):
+        n = len(self.cat)
+        cols = 2
+        figure, ax = plt.subplots(math.ceil(n/cols), cols)
+        r = 0
+        c = 0
+        for col in self.cat:
+            if splitTarg == False:
+                sns.countplot(data=self.data, x=col, ax=ax[r][c])
+            if splitTarg == True:
+                sns.countplot(data=self.data, x=col, hue=self.target, ax=ax[r][c])
+            c += 1
+            if c == cols:
+                r += 1
+                c = 0
+        if show == True:
+            figure.show()
+        return figure
+
+    def histPlots(self, kde=True, splitTarg=False, show=True):
+        n = len(self.num)
+        cols = 2
+        figure, ax = plt.subplots(math.ceil(n/cols), cols)
+        r = 0
+        c = 0
+        for col in self.num:
+            #print("r:",r,"c:",c)
+            if splitTarg == False:
+                sns.histplot(data=self.data, x=col, kde=kde, ax=ax[r][c])
+            if splitTarg == True:
+                sns.histplot(data=self.data, x=col, hue=self.target, kde=kde, ax=ax[r][c])
+            c += 1
+            if c == cols:
+                r += 1
+                c = 0
+        if show == True:
+            figure.show()
+        return figure
+
+    def fullEDA(self):
+        out1 = widgets.Output()
+        out2 = widgets.Output()
+        out3 = widgets.Output()
+        out4 = widgets.Output()
+        out5 = widgets.Output()
+        out6 = widgets.Output()
+        out7 = widgets.Output()
+        out8 = widgets.Output()
+
+        tab = widgets.Tab(children = [out1, out2, out3, out4, out5, out6, out7, out8])
+        tab.set_title(0, 'Info')
+        tab.set_title(1, 'Categorical')
+        tab.set_title(2, 'Numerical')
+        tab.set_title(3, 'Pairplot/Outliers')
+        tab.set_title(4, 'Correlation Matrix')
+        tab.set_title(5, 'Descriptive Statistics')
+        tab.set_title(6, 'Missing Values')
+        tab.set_title(7, 'Target Balance')
+
+        display(tab)
+
+        with out1:
+            self.info()
+
+        with out2:
+            fig2 = self.countPlots(splitTarg=True, show=False)
+            plt.show(fig2)
+        
+        with out3:
+            fig3 = self.histPlots(kde=True, show=False)
+            plt.show(fig3)
+
+        with out4:
+    
+            fig4 = sns.pairplot(self.data)
+            plt.show(fig4)
+
+        with out5:
+            fig5 = sns.heatmap(self.data.corr(), annot=True)
+            plt.show(fig5)
+            
+        with out6:
+            self.describe()
+
+        with out7:
+            print('Count of null values for each feature.\n\n')
+            self.isna()
+
+        with out8:
+            print('Count of samples for categorical target, used to determine if there is inbalance in the data.\n\n')
+            fig8 = sns.countplot(data = self.data, x = self.target)
+            plt.show(fig8)
